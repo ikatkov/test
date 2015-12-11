c9e7dea26fa02af817bacf918d8b09d2499ee383
Igor Katkov <katkovi@yahoo-inc.com>
2015-12-11 15:35:05 -0800

    .

--- client/src/test/java/com/yahoo/omid/SimpleClientTest.java
+++ client/src/test/java/com/yahoo/omid/SimpleClientTest.java
@@ -8,5 +8,7 @@ public class SimpleClientTest {
 
     @Test
     public void testGet() throws Exception {
+        SimpleClient simpleClient = new SimpleClient();
+        simpleClient.get();
     }
 }
